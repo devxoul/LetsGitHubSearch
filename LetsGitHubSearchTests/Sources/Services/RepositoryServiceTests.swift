bf5b0a9ac78332f4eb4cec04657f27ea503ccb69
Suyeol Jeon <devxoul@gmail.com>
2018-11-04 02:09:12 +0900

    테스트 디렉토리 구조 변경

---
+++ LetsGitHubSearchTests/Sources/Services/RepositoryServiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryServiceTests.swift
+//  LetsGitHubSearchTests
+//
+//  Created by Suyeol Jeon on 04/11/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+@testable import LetsGitHubSearch
+
+final class RepositoryServiceTests: XCTestCase {
+}
---
+++ LetsGitHubSearchTests/Sources/ViewControllers/SearchRepositoryViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  SearchRepositoryViewControllerTests.swift
+//  LetsGitHubSearchTests
+//
+//  Created by Suyeol Jeon on 03/11/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+@testable import LetsGitHubSearch
+
+final class SearchRepositoryViewControllerTests: XCTestCase {
+  
+}
