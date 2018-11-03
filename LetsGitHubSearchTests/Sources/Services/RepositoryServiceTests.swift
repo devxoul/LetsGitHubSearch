aa72959429b8610836fc68a274d97e76d85189b1
Suyeol Jeon <devxoul@gmail.com>
2018-11-04 02:11:41 +0900

    RepositoryService.search() 테스트 작성

--- LetsGitHubSearchTests/Sources/Services/RepositoryServiceTests.swift
+++ LetsGitHubSearchTests/Sources/Services/RepositoryServiceTests.swift
@@ -10,4 +10,16 @@ import XCTest
 @testable import LetsGitHubSearch
 
 final class RepositoryServiceTests: XCTestCase {
+  func testSearch_includesSearchResult() {
+    // given
+    let expectation = XCTestExpectation()
+    XCTWaiter().wait(for: [expectation], timeout: 10)
+
+    // then
+    RepositoryService.search(keyword: "RxSwift") { result in
+      expectation.fulfill()
+      XCTAssertEqual(result.isSuccess, true)
+      XCTAssertEqual(result.value?.items.contains(where: { $0.name == "RxSwift" }), true)
+    }
+  }
 }
