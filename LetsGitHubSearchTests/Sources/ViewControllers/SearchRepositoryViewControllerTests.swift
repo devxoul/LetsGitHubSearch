be295a814de0bf017a228d3e229d71223e3bce12
Suyeol Jeon <devxoul@gmail.com>
2018-11-04 20:06:19 +0900

    SearchRepositoryViewController에서 검색을 수행하는 테스트 작성

--- LetsGitHubSearchTests/Sources/ViewControllers/SearchRepositoryViewControllerTests.swift
+++ LetsGitHubSearchTests/Sources/ViewControllers/SearchRepositoryViewControllerTests.swift
@@ -10,5 +10,19 @@ import XCTest
 @testable import LetsGitHubSearch
 
 final class SearchRepositoryViewControllerTests: XCTestCase {
-  
+  func testSearchBar_whenSearchBarSearchButtonClicked_searchWithText() {
+    // given
+    let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SearchRepositoryViewController") as! SearchRepositoryViewController
+    _ = viewController.view
+
+    // when
+    let searchBar = viewController.searchController.searchBar
+    searchBar.text = "ReactorKit"
+    searchBar.delegate?.searchBarSearchButtonClicked?(searchBar)
+
+    // then
+    XCTWaiter().wait(for: [XCTestExpectation()], timeout: 3)
+    let cell = viewController.tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+    XCTAssertEqual(cell?.textLabel?.text, "ReactorKit")
+  }
 }
